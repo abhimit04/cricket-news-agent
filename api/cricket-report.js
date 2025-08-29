6609bda55480821f4fd89aa599b91086673dfca7
abhimit04 <abhimit04@gmail.com>
2025-08-29 15:44:02 +0530

    commit

--- api/cricket-report.js
+++ api/cricket-report.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
 
       // ----------------------------
       // Cricbuzz Latest News
-      // ---------------------------- Abhishek
+      // ---------------------------- Abhishek likes cricket
       const cricbuzzUrl = "https://www.cricbuzz.com/cricket-news";
       const { data: cricbuzzHtml } = await axios.get(cricbuzzUrl);
       const $cb = cheerio.load(cricbuzzHtml);
