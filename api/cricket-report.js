593e6a3dc45480d6e6a1998cc0c361e95c7824c0
abhimit04 <abhimit04@gmail.com>
2025-08-31 00:29:44 +0530

    commit 1

--- api/cricket-report.js
+++ api/cricket-report.js
@@ -143,7 +143,7 @@ export default async function handler(req, res) {
         });
 
         await transporter.sendMail({
-          from: `"Cricket Bot" <${process.env.EMAIL_USER}>`,
+          from: `"Cricket News Agent" <${process.env.EMAIL_USER}>`,
           to: process.env.EMAIL_TO,
           subject: "Daily Cricket Report 🏏",
           html: `
